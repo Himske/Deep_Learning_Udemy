0cfd3f11649bb1a5a89582cfb83ed7276814cc2a
Fredrik Nilsson <himske@gmail.com>
2021-01-29 18:21:00 +0100

    Completed Artificial Neural Network

---
+++ artificial_neural_network/ann.py
@@ -0,0 +1,51 @@
+from inspect import Parameter
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+
+# Import and split dataset
+dataset = pd.read_csv('artificial_neural_network/Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values  # Remove unnecessary columns and result (y)
+y = dataset.iloc[:, -1].values
+
+# Encode categorical data
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])  # Gender
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = ct.fit_transform(X)
+
+# Split dataset into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Scale features
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Build the ANN
+ann = tf.keras.models.Sequential()
+
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))  # first hidden and input layer
+ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
+ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+# Training ANN
+ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+ann.fit(X_train, y_train, batch_size=32, epochs=100, verbose=0)
+
+# Make predictions and evaluate result
+print(ann.predict(sc.transform([[1, 0, 0, 600, 1, 40, 3, 60000, 2, 1, 1, 50000]])) > 0.5)
+
+y_pred = ann.predict(X_test)
+y_pred = (y_pred > 0.5)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+print(accuracy_score(y_test, y_pred))
